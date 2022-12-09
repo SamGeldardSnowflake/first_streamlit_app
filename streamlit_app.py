61d3b3f48076abfbefb2998298934c1503da435d
SamGeldardSnowflake <120112870+SamGeldardSnowflake@users.noreply.github.com>
2022-12-09 11:53:20 +0000

    Update streamlit_app.py
    normalised and made a data frame out of the json file
    removed the line displaying the json file

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# normalizes the above json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# makes a datframe out of the normalized json
+streamlit.dataframe(fruityvice_normalized)
